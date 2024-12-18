e9315601f190bd0c4f115bb9a23ecc167b5ced67
sujandk <sujandhakal787@gmail.com>
2024-12-18 15:00:00 -0600

    dynamic routing handling part I

--- base/views.py
+++ base/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
+rooms = [
+    {'id' : 1 , 'name': 'Lets learn python'},
+    {'id' : 2 , 'name': 'Design with me'},
+    {'id' : 3 , 'name': 'Frontend developers'},
+]
 
 def home(request):
-    return HttpResponse("Home Page")
+    contex = {'rooms' : rooms }
+    return render(request , 'base/home.html' , contex)
 
-def room(request):
-    return HttpResponse("Room")
+def room(request , pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i['name']
+    context = {'room' : room  }
+    return render(request , 'base/room.html' , context)
