ca397ca6d755e677c09aca0d2d064cadaf1cf8ba
sujandk <sujandhakal787@gmail.com>
2024-12-19 16:36:38 -0600

    update and create room done

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     
     path('' , views.home , name ='home'),
     path('room/<str:pk>' , views.room , name= 'room'),
+
+    path('createRoom/' , views.createRoom , name= 'createRoom'),
+    path('updateRoom/<str:pk>' , views.updateRoom , name='updateRoom')
 ]
