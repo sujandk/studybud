613fbba942d445de0df7cb9995184d6b280e3618
sujandk <sujandhakal787@gmail.com>
2024-12-22 00:49:11 -0600

    deletion of messages options

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.decorators  import login_required
-from .models import Room , Topic 
+from .models import Room , Topic , Message
 from django.contrib.auth.models import User
 from .forms import RoomForm
 from django.contrib import messages
@@ -75,7 +75,18 @@ def home(request):
 
 def room(request , pk):
     room = Room.objects.get(id = pk)
-    context = {'room' : room}
+    messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room' , pk = room.id)
+
+    context = {'room' : room , 'messages' : messages, 'participants': participants}
     return render(request , 'base/room.html' , context)
 
 
@@ -115,5 +126,17 @@ def deleteRoom(request , pk):
 
     if request.method == 'POST':
         room.delete()
+        return redirect('home')
+    return render(request , 'base/delete.html' , {'obj': room})
+
+
+@login_required(login_url= 'loginPage')
+def deleteMessage(request , pk):
+    message = Message.objects.get(id = pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to update the room')
+
+    if request.method == 'POST':
+        message.delete()
         return redirect('home')
     return render(request , 'base/delete.html' , {'obj': room})
