c6fe11b29af0a5848affab603b178022a7258761
sujandk <sujandhakal787@gmail.com>
2025-01-02 14:10:54 -0600

    completion of update

--- base/views.py
+++ base/views.py
@@ -116,17 +116,20 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id = pk)
     form = RoomForm(instance = room)
-
+    topic = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed to update the room')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name= request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {'form' : form}
+    context = {'form' : form , 'room' : room, 'topic' : topic}
     return render(request, 'base/createRoom.html' , context)
 
 @login_required(login_url= 'loginPage')
